2dfd9e2ec059be044f8a8f09a2057dd9700586b9
Mihail Stankevich <mihailstankevich@gmail.com>
2024-12-26 14:52:25 +0300

    minor changes

--- moderation/models.py
+++ moderation/models.py
@@ -34,6 +34,7 @@ class DeletedComment(models.Model):
     channel_id = models.CharField(max_length=100)
     deleted_at = models.DateTimeField(auto_now_add=True)
     owner = models.ForeignKey(Owner, on_delete=models.CASCADE, related_name='deleted_comments', null=True)
+    detected_by = models.CharField(max_length=30, null=True, blank=True)
 
     def __str__(self):
         return f"Comment by {self.user} on {self.post}"
