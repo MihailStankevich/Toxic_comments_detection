bf347e07c6c8fa1451dfd203ca9936024e7cd76d
Mihail Stankevich <mihailstankevich@gmail.com>
2025-01-07 16:16:46 +0300

    day 2 trying to make the bot work

--- moderation/ml/__init__.py
+++ moderation/ml/__init__.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import pickle
 import os
+import joblib
 
 # Load model and vectorizer
 def load_model():
@@ -8,7 +9,9 @@ def load_model():
     model_path = os.path.join(base_dir, 'svc1.pkl')  #сейчас это пайплайн который включает в себя и модель и векторайзер
     image_model_path = os.path.join(base_dir, 'spam_image_classifier_model.h5')
     with open(model_path, 'rb') as f:
-        model = pickle.load(f)
+        try: model = joblib.load(f)
+        except Exception as e:
+            print(f"Error loading model: {e}")
     image_model = tf.keras.models.load_model(image_model_path)
     return model, image_model
 
