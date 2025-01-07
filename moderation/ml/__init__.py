d9a50630f31c3516e1650bc8c94eb60adbf93ea6
Mihail Stankevich <mihailstankevich@gmail.com>
2025-01-07 16:23:16 +0300

    day 2 trying to make the bot work1

--- moderation/ml/__init__.py
+++ moderation/ml/__init__.py
@@ -6,8 +6,8 @@ import joblib
 # Load model and vectorizer
 def load_model():
     base_dir = os.path.dirname(os.path.abspath(__file__)) 
-    model_path = os.path.join(base_dir, 'svc1.pkl')  #сейчас это пайплайн который включает в себя и модель и векторайзер
-    image_model_path = os.path.join(base_dir, 'spam_image_classifier_model.h5')
+    model_path = os.path.join(base_dir, 'svc11.pkl')  #сейчас это пайплайн который включает в себя и модель и векторайзер
+    image_model_path = os.path.join(base_dir, 'spam_image_classifier_model11.h5')
     with open(model_path, 'rb') as f:
         try: model = joblib.load(f)
         except Exception as e:
