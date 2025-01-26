a036e2b70c29137b3b7762800f54f0ae59275d1b
Mihail Stankevich <mihailstankevich@gmail.com>
2025-01-26 17:53:42 +0300

    minor changes

--- moderation/bot.py
+++ moderation/bot.py
@@ -10,9 +10,10 @@ from asgiref.sync import sync_to_async
 from telegram import Update
 from telegram.ext import Application, MessageHandler, filters, ContextTypes
 #############
-#  try to do by hashing
+
 #############
 # Set DJANGO_SETTINGS_MODULE
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'channelmoderation.settings')
 django.setup()
 from moderation.ml import model, image_model
