8ee431f8ce0a2f474c3f0b9058ff3e5a7b9c4ffc
Mihail Stankevich <mihailstankevich@gmail.com>
2025-01-31 18:04:59 +0300

    minor

--- moderation/bot.py
+++ moderation/bot.py
@@ -25,7 +25,7 @@ load_dotenv()
 token = os.getenv('TOKEN')
 
 nest_asyncio.apply()
-good = ['irr_i_ssa', 'maxsugarfree', 'xxxxxsssqq', 'igorekbuy', 'frankosha69', 'matras13', 'sacramentozz']
+good = ['irr_i_ssa', 'maxsugarfree', 'xxxxxsssqq', 'igorekbuy', 'frankosha69', 'matras13', 'sacramentozz', 'sd_crown']
 good_id = [1743466232, 7401964075]
 def predict_comment(comment, model):
 
